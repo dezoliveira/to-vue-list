d013bb5acb21008d429aec514b68b5528902b4a9
Andres Oliveira <andres.oliveira@zeeng.com.br>
2022-03-23 23:32:49 -0300

    Edit and delete buttons

--- to-vue-list/src/components/List.vue
+++ to-vue-list/src/components/List.vue
@@ -3,8 +3,8 @@
     <h1 class="text-center m-4">To Vue List</h1>
 
     <div class="d-flex m-4">
-      <input type="text" placeholder="Enter Task" class="form-control rounded-0">
-      <button class="btn btn-success rounded-0">Pesquisar</button>
+      <input v-model="task" type="text" placeholder="Enter Task" class="form-control rounded-0">
+      <button @click="submitTask" class="btn btn-success rounded-0">Submit</button>
     </div>
 
     <!-- Table Bootstrap -->
@@ -18,18 +18,18 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Steal banana from the store</td>
-        <td>To do</td>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td>{{task.name}}</td>
+        <td>{{task.status}}</td>
         <td>
-          <div class="text-center">
+          <div class="text-center" @click="editTask(index)">
             <span>
               <i class="fa fa-pen"></i>
             </span>
           </div>
         </td>
         <td>
-          <div class="text-center">
+          <div class="text-center" @click="deleteTask(index)">
             <span>
               <i class="fa fa-trash"></i>
             </span>
@@ -47,11 +47,59 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data(){
+    return{
+      task: '',
+      editedTask: null,
+      tasks: [
+        {
+          name: 'Steal banana from the store',
+          status: 'to-do',
+        },
+        {
+          name: 'Eat 1kg of chocolate',
+          status: 'in-progress'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    submitTask(){
+      if(this.task.length === 0) return;
+
+      if(this.editedTask === null){
+        this.tasks.push({
+          name: this.task,
+          status: 'to-do'
+        })
+      }else{
+        this.tasks[this.editedTask].name = this.task
+        this.editedTask = null
+      }
+
+      this.task = ''
+    },
+
+    deleteTask(index){
+      this.tasks.splice(index, 1)
+    },
+
+    editTask(index){
+      this.task = this.tasks[index].name
+      this.editedTask = index
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .form-control:focus {
+  border-color: green;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
 </style>
